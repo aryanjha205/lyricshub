3fd5cd7c7a0723da49c45b64dd44d908736132a7
Aryan jha <152834383+aryanjha205@users.noreply.github.com>
2025-01-06 18:03:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,103 +7,111 @@ let gif = document.getElementById('gif');
 let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
+// Song list data with correct durations (in seconds)
 let songs = [
-    {songName: "Gulabi Ankhiyan,Jubin Nautiyal", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Jeena Haraam,Vishal Mishra", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "Tiranga, B Praak", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Akhiyaan Gulaab,Mitraz", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Tum Se,Raghav Chaitanya", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Hanuman Karenge Kalyan,Hansraj", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Kamsin Kali, Tony Kakkar", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
-    {songName: " Ghagra,  Romy", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
-    {songName: " Maroon Color Sadiya,Om jha", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
-    
-    
+    {songName: "Gulabi Ankhiyan, Jubin Nautiyal, Sakshi Holkar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg", duration: 170}, // 2:50
+    {songName: "Jeena Haraam, Tanishk Bagchi, Vishal Mishra, Shilpa Rao", filePath: "songs/2.mp3", coverPath: "covers/2.jpg", duration: 210}, // 3:30
+    {songName: "Tiranga, B Praak", filePath: "songs/3.mp3", coverPath: "covers/3.jpg", duration: 240}, // 4:00
+    {songName: "Akhiyaan Gulaab, Mitraz", filePath: "songs/4.mp3", coverPath: "covers/4.jpg", duration: 200}, // 3:20
+    {songName: "Tum Se, Raghav Chaitanya, Sachin Jigar, and Varun Jain", filePath: "songs/5.mp3", coverPath: "covers/5.jpg", duration: 185}, // 3:05
+    {songName: "Hanuman Karenge Kalyan, Hansraj Raghuwanshi", filePath: "songs/6.mp3", coverPath: "covers/6.jpg", duration: 230}, // 3:50
+    {songName: "Kamsin Kali, Tony Kakkar, Neha Kakkar", filePath: "songs/7.mp3", coverPath: "covers/7.jpg", duration: 220}, // 3:40
+    {songName: "Ghagra, Romy, Srushti Tawade", filePath: "songs/8.mp3", coverPath: "covers/8.jpg", duration: 210}, // 3:30
+    {songName: "Maroon Color Sadiya, Om Jha", filePath: "songs/9.mp3", coverPath: "covers/9.jpg", duration: 200} // 3:20
 ];
 
+// Update song items with cover images and names
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
 
-songItems.forEach((element, i)=>{ 
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
-})
+    // Format and set the timestamp for each song
+    let minutes = Math.floor(songs[i].duration / 60);
+    let seconds = songs[i].duration % 60;
+    if (seconds < 10) {
+        seconds = '0' + seconds; // Add leading zero for seconds
+    }
 
+    element.getElementsByClassName("timestamp")[0].innerText = `${minutes}:${seconds}`;
+});
 
 // Handle play/pause click
-masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
         gif.style.opacity = 1;
-    }
-    else{
+    } else {
         audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');
         gif.style.opacity = 0;
     }
-})
-// Listen to Events
-audioElement.addEventListener('timeupdate', ()=>{ 
-    // Update Seekbar
-    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+});
+
+// Update progress bar as audio plays
+audioElement.addEventListener('timeupdate', () => {
+    let progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
     myProgressBar.value = progress;
-})
+});
 
-myProgressBar.addEventListener('change', ()=>{
-    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
-})
+// Update audio current time based on progress bar change
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+});
 
-const makeAllPlays = ()=>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+// Reset all play buttons to play state
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
         element.classList.remove('fa-pause-circle');
         element.classList.add('fa-play-circle');
-    })
-}
+    });
+};
 
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click', (e)=>{ 
-        makeAllPlays();
-        songIndex = parseInt(e.target.id);
+// Handle play button click for each song item
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element, index) => {
+    element.addEventListener('click', (e) => {
+        makeAllPlays(); // Reset play icons
+        songIndex = index; // Set the song index based on the clicked item
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
-        audioElement.src = `songs/${songIndex+1}.mp3`;
-        masterSongName.innerText = songs[songIndex].songName;
-        audioElement.currentTime = 0;
-        audioElement.play();
-        gif.style.opacity = 1;
+        audioElement.src = songs[songIndex].filePath; // Set the audio source
+        masterSongName.innerText = songs[songIndex].songName; // Update the song name
+        audioElement.currentTime = 0; // Reset audio time to start
+        audioElement.play(); // Play the song
+        gif.style.opacity = 1; // Show gif (indicating the song is playing)
         masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-    })
-})
+        masterPlay.classList.add('fa-pause-circle'); // Update the master play button
+    });
+});
 
-document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex>=9){
-        songIndex = 0
+// Handle next button click (play next song)
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= songs.length - 1) {
+        songIndex = 0; // Go to the first song
+    } else {
+        songIndex++; // Go to the next song
     }
-    else{
-        songIndex += 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
+    audioElement.src = songs[songIndex].filePath; // Set the audio source
+    masterSongName.innerText = songs[songIndex].songName; // Update song name
+    audioElement.currentTime = 0; // Reset the song time
+    audioElement.play(); // Play the song
     masterPlay.classList.remove('fa-play-circle');
     masterPlay.classList.add('fa-pause-circle');
+});
 
-})
-
-document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
-        songIndex = 0
+// Handle previous button click (play previous song)
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
+        songIndex = songs.length - 1; // Go to the last song
+    } else {
+        songIndex--; // Go to the previous song
     }
-    else{
-        songIndex -= 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
+    audioElement.src = songs[songIndex].filePath; // Set the audio source
+    masterSongName.innerText = songs[songIndex].songName; // Update song name
+    audioElement.currentTime = 0; // Reset the song time
+    audioElement.play(); // Play the song
     masterPlay.classList.remove('fa-play-circle');
     masterPlay.classList.add('fa-pause-circle');
-})
+});
